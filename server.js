150eee521077d804496cb1615e9d387112fa4f24
James Fuller <jblotus@gmail.com>
2011-05-29 22:26:55 -0400

    tweak

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ http.createServer(function (req, res) {
   //wrap calls in a try catch or the node js server will crash upon any code errors
   try {
     //pipe some details to the node console
-    console.log('Incoming Request from : ' + req.connection.remoteAddress + ' for href: ' + url.parse(req.url).href);
+    console.log('Incoming Request from: ' + req.connection.remoteAddress + ' for href: ' + url.parse(req.url).href);
     
     //dispatch our request
     dispatcher.dispatch(req, res); 
