e639f886764333aea54b17f9dbf3f7154b2c349a
James Fuller <jblotus@gmail.com>
2011-05-29 22:21:20 -0400

    added some debug stuff to basic web server

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 //include our modules
 var sys  = require('sys');
 var http = require('http');
+var url = require('url');
 
 //require custom dispatcher
 var dispatcher = require('./lib/dispatcher.js');
 
+console.log('Starting server @ http://127.0.0.1:1337/');
+
 http.createServer(function (req, res) {
   //wrap calls in a try catch or the node js server will crash upon any code errors
   try {
+    //pipe some details to the node console
+    console.log('Incoming Request from : ' + req.connection.remoteAddress + ' for href: ' + url.parse(req.url).href);
+    
+    //dispatch our request
     dispatcher.dispatch(req, res); 
     
   } catch (err) {
+    //handle errors gracefully
     sys.puts(err);
     res.writeHead(500);
     res.end('Internal Server Error');
   }  
    
-}).listen(1337, "127.0.0.1");
-
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(1337, "127.0.0.1", function() {
+  //runs when our server is created
+  console.log('Server running at http://127.0.0.1:1337/');
+});
