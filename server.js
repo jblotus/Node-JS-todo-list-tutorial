94605f52a6712d4bf4e6da3e7130f1e855c729fa
James Fuller <jblotus@gmail.com>
2011-05-29 22:05:16 -0400

    wrapped main node server call in a try catch

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ var http = require('http');
 //require custom dispatcher
 var dispatcher = require('./lib/dispatcher.js');
 
-http.createServer(function (req, res) {  
-  dispatcher.dispatch(req, res);  
+http.createServer(function (req, res) {
+  //wrap calls in a try catch or the node js server will crash upon any code errors
+  try {
+    dispatcher.dispatch(req, res); 
+    
+  } catch (err) {
+    sys.puts(err);
+    res.writeHead(500);
+    res.end('Internal Server Error');
+  }  
+   
 }).listen(1337, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');
