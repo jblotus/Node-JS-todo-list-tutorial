9cf9669957fa1b66f3007ef8423e3d952dbef877
James Fuller <jblotus@gmail.com>
2011-06-04 14:14:13 -0400

    renamed js directory

--- app/views/layouts/default.html.js
+++ app/views/layouts/default.html.js
@@ -7,6 +7,9 @@
 <body>
 	<h1>Todo list in Node</h1>
 	
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.1.min.js">\x3C/script>')</script>
 	<script src="/javascript/script.js"></script>
 </body>
 </html>
