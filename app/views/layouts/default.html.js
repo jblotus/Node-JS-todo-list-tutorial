25a59aa134e4f48262c3f6f662cfb5e7d5ac4b21
James Fuller <jblotus@gmail.com>
2011-06-04 14:10:21 -0400

    added some basic template stuff

--- app/views/layouts/default.html.js
+++ app/views/layouts/default.html.js
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
-
- <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-       Remove this if you use the .htaccess -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-  <title></title>
+  <title>Todo List in Node</title>
   <link rel="stylesheet" href="/css/style.css">
-
 </head>
 <body>
 	<h1>Todo list in Node</h1>
+	
+	<script src="/javascript/script.js"></script>
 </body>
 </html>
