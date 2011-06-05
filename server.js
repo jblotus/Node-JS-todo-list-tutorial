303a923cce27865223836994ce9f4d4ea8ab20c4
James Fuller <jblotus@gmail.com>
2011-06-04 21:33:28 -0400

    refactored all components

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-//include our modules
+// in other modules
 var sys  = require('sys');
 var http = require('http');
-var url = require('url'); 
+var url = require('url');
 
 //require custom dispatcher
 var dispatcher = require('./lib/dispatcher.js');
@@ -13,17 +13,17 @@ http.createServer(function (req, res) {
   try {
     //pipe some details to the node console
     console.log('Incoming Request from: ' + req.connection.remoteAddress + ' for href: ' + url.parse(req.url).href);
-    
+
     //dispatch our request
-    dispatcher.dispatch(req, res); 
-    
+    dispatcher.dispatch(req, res);
+
   } catch (err) {
     //handle errors gracefully
     sys.puts(err);
     res.writeHead(500);
     res.end('Internal Server Error');
-  }  
-   
+  }
+
 }).listen(1337, "127.0.0.1", function() {
   //runs when our server is created
   console.log('Server running at http://127.0.0.1:1337/');
