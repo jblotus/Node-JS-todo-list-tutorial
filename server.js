d5088c2928065426bd554363b0e72cac87e1fad4
James Fuller <jblotus@gmail.com>
2011-06-18 12:42:22 -0400

    removed files

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var url  = require('url');
 var dispatcher = require('./lib/dispatcher.js');
 
 console.log('Starting server @ http://127.0.0.1:1337/');
-
+zxc
 http.createServer(function (req, res) {
   //wrap calls in a try catch or the node js server will crash upon any code errors
-  try {
+  try {asd
     //pipe some details to the node console
     console.log('Incoming Request from: ' + req.connection.remoteAddress + ' for href: ' + url.parse(req.url).href);
 
